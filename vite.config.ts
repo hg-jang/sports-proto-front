b8daa13a81423d52ec357d320a33074f9d107034
hg-jang <wkdgusrhkd@gmail.com>
2023-06-04 03:11:41 +0900

    [Feat]: 절대 경로 import 설정 추가 - npm install url, path - vite, tsconfig 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
+// @ts-nocheck
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
 });
