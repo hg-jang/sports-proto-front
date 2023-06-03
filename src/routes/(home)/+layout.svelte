47143f18410e24af2e6bd0e336814258396e9ac6
hg-jang <wkdgusrhkd@gmail.com>
2023-06-03 23:29:02 +0900

    [Feat]: 홈 레이아웃 마크업 임시 완성

--- src/routes/(home)/+layout.svelte
+++ src/routes/(home)/+layout.svelte
@@ -1,4 +1,33 @@
+<script>
+	import './home-layout.css'
+</script>
+
 <div class="home">
-	<h1>It's home layout</h1>
-	<slot />
+	<nav class="bg-violet-500 text-white">
+		<div class="wrapper flex flex-wrap items-center justify-between px-4">
+			<a href="/" class="flex items-center">
+				<span class="text-2xl text-white font-semibold whitespace-nowrap">
+					Logo
+				</span>
+			</a>
+			<ul class="flex m-0 p-0 list-none leading-[100%]">
+				<li class="p-4 hover:bg-orange-300 cursor-pointer">Home</li>
+				<li class="p-4 hover:bg-orange-300 cursor-pointer">Menu 1</li>
+				<li class="p-4 hover:bg-orange-300 cursor-pointer">Menu 2</li>
+				<li class="p-4 hover:bg-orange-300 cursor-pointer">Log In</li>
+			</ul>
+		</div>
+	</nav>
+
+	<main class="p-16">
+		<div class="wrapper">
+			<slot />
+		</div>
+	</main>
+ 
+	<footer class="p-16 bg-violet-400">
+		<div class="wrapper">
+			It's Footer
+		</div>
+	</footer>
 </div>
