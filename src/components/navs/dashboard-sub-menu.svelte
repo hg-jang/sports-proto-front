bfb04baa7d191e64767168fc7bb341d9713ea16b
hg-jang <wkdgusrhkd@gmail.com>
2023-06-06 01:02:44 +0900

    [Feat]: dashboard-sub-menu 컴포넌트 생성

---
+++ src/components/navs/dashboard-sub-menu.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import FilledButton from '~/components/buttons/filled-button.svelte';
+
+  export let subMenuGroups: {
+    text: string
+    path: string
+    color: string
+  }[][]
+
+  // const subMenuGroups = [
+	// 	[
+	// 		{
+	// 			text: '자유게시판',
+	// 			path: '/board',
+	// 		}, {
+	// 			text: 'Q & A',
+	// 			path: '/question',
+	// 		}
+	// 	], [
+	// 		{
+	// 			text: '자유게시판',
+	// 			path: '/board',
+	// 		}, {
+	// 			text: 'Q & A',
+	// 			path: '/question',
+	// 		}
+	// 	]
+	// ]
+</script>
+
+<div class="flex gap-4">
+  {#each subMenuGroups as subMenuGroup, idx}
+    <div class="flex gap-2">
+      {#each subMenuGroup as subMenu}
+        <FilledButton color='primary' text={subMenu.text} size='xs' />
+      {/each}
+    </div>
+
+    {#if idx !== subMenuGroups.length - 1}
+      <div class="my-1 w-0.5 bg-black"></div>
+    {/if}
+  {/each}
+</div>
