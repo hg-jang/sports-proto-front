54e571fe7dd848c4f001d00ee0b394c53dcb8473
hg-jang <wkdgusrhkd@gmail.com>
2023-06-06 00:34:44 +0900

    [Fix]: tailwind config safelist 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,12 @@ export default {
   corePlugins: {
     preflight: false,
   },
+  safelist: [
+    'text-xs', 'text-sm', 'text-base', 'text-lg',
+    {
+      pattern: /bg-(red|green|blue|purple)-(100|200|300|400|500|600|700|800|900)/,
+      variants: ['lg', 'hover', 'focus', 'lg:hover'],
+    },
+  ],
 }
 
