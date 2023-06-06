c7b249c8a03976d88bffa0970ea818f5d1e7ea34
hg-jang <wkdgusrhkd@gmail.com>
2023-06-07 00:12:29 +0900

    [Fix]: 대쉬보드 서브메뉴 컴포넌트에 버튼 핸들러 추가

--- src/components/navs/dashboard-sub-menu.svelte
+++ src/components/navs/dashboard-sub-menu.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
+  import { goto } from '$app/navigation';
+
   import FilledButton from '~/components/buttons/filled-button.svelte';
   import type { SubMenu } from '~/lib/mock/data';
 
   export let subMenuGroups: SubMenu[][]
+
+  function subMenuButtonHandler(path: string) {
+    if(path.startsWith('/')) {
+      path = path.substring(1);
+    }
+    
+    goto(`/name/${path}`)
+  }
 </script>
 
 <div class="flex gap-4">
   {#each subMenuGroups as subMenuGroup, idx}
     <div class="flex gap-2">
       {#each subMenuGroup as subMenu}
-        <FilledButton color={subMenu.color} text={subMenu.menu} size='xs' />
+        <FilledButton color={subMenu.color} text={subMenu.menu} size='xs' handler={() => subMenuButtonHandler(subMenu.path)} />
       {/each}
     </div>
 
