7f6c7633db90a757f809622179af91f10869bd44
hg-jang <wkdgusrhkd@gmail.com>
2023-06-07 23:49:49 +0900

    [Fix]: 뱃지 컴포넌트 스타일 수정

--- src/components/badges/badge.svelte
+++ src/components/badges/badge.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
   export let text: string;
+  export let color: string;
 </script>
 
 <span
   class="
     px-2 py-0.5
-    bg-blue-100 text-blue-800 text-xs font-medium
-    rounded
-    dark:bg-blue-900 dark:text-blue-300
+    bg-{color}-100 text-blue-800 text-xs font-medium
+    rounded break-keep
+    dark:bg-{color}-900 dark:text-blue-300
   "
 >
   {text}
