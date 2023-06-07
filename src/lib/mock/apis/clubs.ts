00dd895e0376849d395e916232ef8305243efce1
hg-jang <wkdgusrhkd@gmail.com>
2023-06-08 01:20:39 +0900

    [Feat]: club mock api - GET_MY_CLUBS

--- src/lib/mock/apis/clubs.ts
+++ src/lib/mock/apis/clubs.ts
@@ -22,3 +22,17 @@ export function GET_CLUB_BY_ID(clubId: number) {
     }
   })
 }
+
+/**
+ * user id로 내가 가입한 클럽의 id와 name 가져오기
+ */
+export function GET_MY_CLUBS(userId: number) {
+  return new Promise<Pick<Club, 'id' | 'name'>[]>((resolve, reject) => {
+    const clubs = CLUBS.filter((c) => c.members.includes(userId))
+    if(clubs) {
+      resolve(clubs)
+    } else {
+      reject('Not found')
+    }
+  })
+}
