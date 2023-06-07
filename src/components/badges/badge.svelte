bc5ba6ca660b1a566cef2db7107c109d99ee4d03
hg-jang <wkdgusrhkd@gmail.com>
2023-06-07 23:40:05 +0900

    [Feat]: 뱃지 컴포넌트 생성

---
+++ src/components/badges/badge.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let text: string;
+</script>
+
+<span
+  class="
+    px-2 py-0.5
+    bg-blue-100 text-blue-800 text-xs font-medium
+    rounded
+    dark:bg-blue-900 dark:text-blue-300
+  "
+>
+  {text}
+</span>
