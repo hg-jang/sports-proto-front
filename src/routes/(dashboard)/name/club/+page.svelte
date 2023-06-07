45112880173a02e59e5f9f7cf8cd1b53592516c6
hg-jang <wkdgusrhkd@gmail.com>
2023-06-08 00:17:25 +0900

    [Fix]: 뱃지 호출 테스트

--- src/routes/(dashboard)/name/club/+page.svelte
+++ src/routes/(dashboard)/name/club/+page.svelte
@@ -1,34 +1,28 @@
 <script lang="ts">
-  import DashboardLayout from "~/components/custom-layouts/dashboard-layout.svelte";
-  import DashboardSubMenu from "~/components/navs/dashboard-sub-menu.svelte";
   import PlainInput from "~/components/inputs/plain-input.svelte";
   import ClubCard from "~/components/cards/club-card.svelte";
-  
-  import type { SubMenu } from "~/lib/mock/data";
+  import Badge from "~/components/badges/badge.svelte";
 
   import type { PageData } from "./$types";
 
   export let data: PageData
 
-  const subMenuGroups: SubMenu[][] = [
-    [
-      {
-        menu: '클럽 가입',
-        path: '/club',
-        color: 'primary',
-      }, {
-        menu: '클럽 생성',
-        path: '/club/new',
-        color: 'primary',
-      }
-    ]
-  ]
   let clubName = ''
 </script>
 
 <div class="grid grid-rows-1 grid-cols-12 gap-4 h-full text-black">
   <div class="col-span-4 p-2 rounded-md dark:bg-slate-300">
-    <PlainInput id='clubName' label='클럽 검색' placeholder='클럽명' bind:value={clubName} />
+    <div class="mb-4">
+      <PlainInput id='clubName' label='클럽 검색' placeholder='클럽명' bind:value={clubName} />
+    </div>
+
+    <div class="flex flex-wrap gap-2">
+      <Badge text='상도동' color='blue' />
+      <Badge text='2030' color='orange' />
+      <Badge text='3040' color='red' />
+      <Badge text='테친놈들' color='green' />
+      <Badge text='테린이들' color='yellow' />
+    </div>
   </div>
 
   <div class="col-span-8 p-4 rounded-md dark:bg-slate-300">
